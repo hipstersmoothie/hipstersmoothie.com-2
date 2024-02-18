755ff4266c8800d2b7d946ef9399f78d4a11f652
Andrew Lisowski <lisowski54@gmail.com>
2024-02-18 15:30:51 -0800

    remove mdxRs

--- next.config.mjs
+++ next.config.mjs
@@ -151,9 +151,6 @@ const withMDX = nextMdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  experimental: {
-    mdxRs: true,
-  },
 };
 
 export default withMDX(nextConfig);
